6bdc9c96a234890c2d73d981eb47bcd0f309c410
Simon Garnier <simongarnier07@hotmail.fr>
2016-06-29 13:54:54 +0200

    Render counts with events

--- restAPI/app/models/EventsDAO.scala
+++ restAPI/app/models/EventsDAO.scala
@@ -22,7 +22,8 @@ class EventsDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider
           (eventsArtists join artists on (_.artistId === _.facebookId)) on (_._1.facebookId === _._1.eventId) joinLeft
           (eventsPlaces join places on (_.placeFacebookUrl === _.facebookUrl)) on
             (_._1._1.facebookId === _._1.eventId) joinLeft
-          (eventsAddresses join addresses on (_.addressId === _.id)) on (_._1._1._1.facebookId === _._1.eventId)
+          (eventsAddresses join addresses on (_.addressId === _.id)) on (_._1._1._1.facebookId === _._1.eventId) joinLeft
+          eventsCounts on (_._1._1._1._1.facebookId === _.eventFacebookId)
     } yield (eventWithOptionalEventOrganizers, optionalEventArtists, optionalEventPlaces, optionalEventAddresses)
 
     db.run(query.result) map(eventWithRelations =>
@@ -30,35 +31,39 @@ class EventsDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider
   }
 
   private def eventWithRelationsTupleToEventWithRelation(eventWithRelations:
-                                                         Seq[((Event, Option[(EventOrganizerRelation, Organizer)]),
-                                                           Option[(EventArtistRelation, Artist)],
+                                                         Seq[(((Event, Option[(EventOrganizerRelation, Organizer)]),
+                                                           Option[(EventArtistRelation, Artist)]),
                                                            Option[(EventPlaceRelation, Place)],
-                                                           Option[(EventAddressRelation, Address)])])
-  : Vector[EventWithRelations] = {
+                                                           Option[(EventAddressRelation, Address)],
+                                                           Option[Counts])]): Vector[EventWithRelations] = {
     val groupedByEvents = eventWithRelations.groupBy(_._1._1)
 
     groupedByEvents.map { eventWithOptionalRelations =>
-      val event = eventWithOptionalRelations._1
+      val event = eventWithOptionalRelations._1._1
       val relations = eventWithOptionalRelations._2
       val organizers = (relations collect {
-        case ((_, Some((_, organizer: Organizer))), _, _, _) => organizer
+        case (((_, Some((_, organizer: Organizer))), _), _, _, _) => organizer
       }).distinct
       val artists = (relations collect {
-        case ((_, _), Some((_, artist: Artist)), _, _) => artist
+        case (((_, _), Some((_, artist: Artist))), _, _, _) => artist
       }).distinct
       val places = (relations collect {
-        case ((_, _), _, Some((_, place: Place)), _) => place
+        case (((_, _), _), Some((_, place: Place)), _, _) => place
       }).distinct
       val addresses = (relations collect {
-        case ((_, _), _, _, Some((_, address: Address))) => address
+        case (((_, _), _), _, Some((_, address: Address)), _) => address
       }).distinct
+      val counts = (relations collect {
+        case (((_, _), _), _, _, Some(counts: Counts)) => counts
+      }).headOption
 
       EventWithRelations(
         event,
         organizers map (OrganizerWithAddress(_)),
         artists,
         places map (PlaceWithAddress(_)),
-        addresses)
+        addresses,
+        counts)
     }.toVector
   }
 }
