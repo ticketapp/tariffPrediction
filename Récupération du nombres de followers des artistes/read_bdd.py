211e51d8889bcab1dfd0aeca4abda523312e3a0c
mewain <ropersmerwan@gmail.com>
2016-09-10 18:48:36 +0900

    Update read_bdd.py

--- Récupération du nombres de followers des artistes/read_bdd.py
+++ Récupération du nombres de followers des artistes/read_bdd.py
@@ -3,19 +3,19 @@
 # Merwan ROPERS
 #
 # Script permettant de parcourir la base de données (https://claude.wtf/artists?numberToReturn=1&offset=1)
-# et d'en extraire le nom de l'artiste ainsi que son site web renseigné.
+# et d'en extraire le nom de l'artiste son site web renseigné, et l'ID facebook de l'évènement.
 #
 # Mode d'emploi : 
 # Argument : Numéro de l'artiste sur la base de données (facultatif, 0 par défaut)
 # 	$ ./read_bdd.py 
 # 	$ ./read_bdd.py 42
 # Valeur retournée : 
-#	Tableau contenant le nom de l'artiste [0] et son site web [1].
+#	Tableau contenant le nom de l'artiste [0] son site web [1], et l'ID Facebook de l'évènement [2].
 #	La valeur 'None' est retournée si l'artiste n'as pas renseigné son site web.
-# 		(u'Nevercold', u'nevercoldmetal.com')
+# 		(u'Nevercold', u'nevercoldmetal.com', u'90283121490')
 # 		(u'POINT G', 'None')
 #	En cas d'erreur du chargement de la page ou autre : GET_ERROR [0] et [1].
-#		('GET_ERROR', 'GET_ERROR')
+#		('GET_ERROR', 'GET_ERROR', 'GET_ERROR')
 #
 #	Le script effectuera un print uniquement si celui-ci est lancé directement. (pour les phases de test)
 
@@ -32,7 +32,7 @@ def bdd_extract(artiste_num) :
 		except ValueError : return "GET_ERROR","GET_ERROR"
 		if len(web_page) == 0 : return "GET_ERROR","GET_ERROR"
 		if len(web_page[0]['artist']['websites']) != 0  : 
-			return(web_page[0]['artist']['name'], web_page[0]['artist']['websites'][0])
+			return(web_page[0]['artist']['name'], web_page[0]['artist']['websites'][0], web_page[0]['artist']['facebookId'])
 		else : 
 			web_page[0]['artist']['websites'].append("None")
 			return web_page[0]['artist']['name'], web_page[0]['artist']['websites'][0]
